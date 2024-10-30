839700d832bad7942a3c139a6381da515d195e24
AKira0169 <odayterk75@gmail.com>
2024-10-31 00:17:01 +0300

    adding test

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,18 @@
 import { usePosts } from '../components/PostContext';
+import Test from '../Test';
 export function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      <Test />
+    </>
   );
 }
